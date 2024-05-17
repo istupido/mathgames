954808159febd9af74dac954e13191c066277dd7
istupido <164699282+istupido@users.noreply.github.com>
2024-05-17 17:32:09 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://collegeapp.me/v/',
+    bare: 'https://dm-unbl0cker-bare-server-nu.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
